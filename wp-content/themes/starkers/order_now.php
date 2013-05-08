270a21126fa972bad10658594da8cf268adffde6
KeVon <kevtice15@gmail.com>
2013-05-07 21:39:02 -0400

    order_now page populates from db now

--- wp-content/themes/starkers/order_now.php
+++ wp-content/themes/starkers/order_now.php
@@ -3,14 +3,35 @@
 Template Name: Order Now
 */
 ?>
-<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
+<?php 
+	Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); 
+	//echo do_shortcode("[ic_add_posts post_type='restaurant']");
+?>
+
+
+
 <div id="container">
 	<div id="container-inner-order">
     	<ul>
-        	<li><a href="#"><div class="order-outer"><div class="round"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/vietnamese.png" class="nnationpic"></img></div><span>Vietnamese</span></div></a></li>
-            <li><a href="#"><div class="order-outer"><div class="round"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/thailand.png" class="nnationpic"></img></div><span>Thai</span></div></a></li>
-            <li><a href="#"><div class="order-outer"><div class="round"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/india.png" class="nnationpic"></img></div><span>Indian</span></div></a></li>
-            <li><a href="#"><div class="order-outer"><div class="round"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/chinese.png" class="nnationpic"></img></div><span>Chinese</span></div></a></li>
+    		<?php
+    			$type = 'restaurant';
+    			$args = array(
+    				'post_type' => $type,
+    				'post_status' => 'publish',
+    				'posts_per_page' => -1,
+    				'caller_get_posts' => 1
+    			);
+    			$query = null;
+    			$query = new WP_Query($args);
+    			if($query->have_posts()){
+    				while($query->have_posts()) : $query->the_post(); ?>
+    				<?php	
+    				echo '<li><a href="#"><div class="order-outer"><div class="round"><img src=' . get_stylesheet_directory_uri() . '/img/vietnamese.png class="nnationpic"></img></div><span>' . get_the_title() . '</span></div></a></li>';
+    				endwhile;
+    				}
+    				wp_reset_query();
+    				wp_reset_postdata();
+    		?>
         </ul>
     </div>
 </div>
